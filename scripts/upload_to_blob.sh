ee5d122653a79e671a8b6d9e74a89ca6f6e52bc1
skr003 <78012061+skr003@users.noreply.github.com>
2025-09-11 01:41:21 +0530

    Update upload_to_blob.sh

--- scripts/upload_to_blob.sh
+++ scripts/upload_to_blob.sh
@@ -5,7 +5,6 @@ STORAGE_ACCOUNT="cloudriskengine"
 CONTAINER="reports"
 STORAGE_ACCOUNT_KEY="$STORAGE_ACCOUNT_KEY"
 for file in drift_report.json remediation.json remediate.sh privilege_graph.json; do
-  az ad user list
   az storage blob upload --account-name $STORAGE_ACCOUNT --container-name $CONTAINER --file output/$file --name builds/$BUILD_NUMBER/$file --account-key "$STORAGE_ACCOUNT_KEY" --overwrite
   az storage blob upload --account-name $STORAGE_ACCOUNT --container-name $CONTAINER --file output/$file --name latest/$file --account-key "$STORAGE_ACCOUNT_KEY" --overwrite
   echo "✅ Uploaded $file"
