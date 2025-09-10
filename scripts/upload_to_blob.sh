db44ea6ff9bda0cde11fd2a623652afb3c77b94e
skr003 <78012061+skr003@users.noreply.github.com>
2025-09-11 01:14:18 +0530

    Update upload_to_blob.sh

--- scripts/upload_to_blob.sh
+++ scripts/upload_to_blob.sh
@@ -6,5 +6,6 @@ CONTAINER="reports"
 STORAGE_ACCOUNT_KEY="$STORAGE_ACCOUNT_KEY"
 for file in drift_report.json remediation.json remediate.sh privilege_graph.json; do
   az storage blob upload --account-name $STORAGE_ACCOUNT --container-name $CONTAINER --file output/$file --name builds/$BUILD_NUMBER/$file --account-key "$STORAGE_ACCOUNT_KEY" --overwrite
+  az storage blob upload --account-name $STORAGE_ACCOUNT --container-name $CONTAINER --file output/$file --name latest/$file --account-key "$STORAGE_ACCOUNT_KEY" --overwrite
   echo "✅ Uploaded $file"
 done
